ecb8a9b4391f4eeb1baf1fd54320952440157bf8
Dustin Crossley <kuark7@hotmail.com>
2017-09-21 22:53:07 -0700

    End of Lesson 2

--- Math Quiz/Form1.cs
+++ Math Quiz/Form1.cs
@@ -12,9 +12,40 @@ namespace Math_Quiz
 {
     public partial class Form1 : Form
     {
+        // Random object to generate random numbers.
+        Random randomizer = new Random();
+
+        // To Store numbers for addition problem.
+        int addend1;
+        int addend2;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Start quiz by filling in all the problems
+        /// and starting timer
+        /// </summary>
+        public void StartTheQuiz()
+        {
+            // Fill in the addition problem
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            // Convert random numbers to strings to display in label controls
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            // initialize sum value
+            sum.Value = 0;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
