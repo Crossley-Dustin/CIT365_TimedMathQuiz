43c1436072bf31131222e2b9736ace28ce384192
Dustin Crossley <kuark7@hotmail.com>
2017-09-21 23:04:11 -0700

    End of Step 3.

--- Math Quiz/Form1.cs
+++ Math Quiz/Form1.cs
@@ -19,6 +19,9 @@ namespace Math_Quiz
         int addend1;
         int addend2;
 
+        // Keep track of remaining time
+        int timeLeft;
+
         public Form1()
         {
             InitializeComponent();
@@ -26,26 +29,51 @@ namespace Math_Quiz
 
         /// <summary>
         /// Start quiz by filling in all the problems
-        /// and starting timer
+        /// and starting timer.
         /// </summary>
         public void StartTheQuiz()
         {
-            // Fill in the addition problem
+            // Fill in the addition problem.
             addend1 = randomizer.Next(51);
             addend2 = randomizer.Next(51);
 
-            // Convert random numbers to strings to display in label controls
+            // Convert random numbers to strings to display in label controls.
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
 
-            // initialize sum value
+            // Initialize sum value.
             sum.Value = 0;
+
+            // Start the timer.
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
             StartTheQuiz();
             startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                // Display remaining time in Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If user ran out of time, stop timer,
+                // show a MessageBox, and fill in answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                startButton.Enabled = true;
+            }
         }
     }
 }
